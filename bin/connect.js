0c186d0fbf335f41971c43ae780ffdeeaf1bc19a
Shah Kashani <skashani@gmail.com>
2018-03-03 18:13:18 -0500

    Add Tumblr support

--- bin/connect.js
+++ bin/connect.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const dropbox = require('../lib/dropbox');
 const twitter = require('../lib/twitter');
+const tumblr = require('../lib/tumblr');
 const _ = require('lodash');
 
 dropbox
@@ -16,13 +17,28 @@ dropbox
     console.log('Dropbox connection failed', err);
   });
 
-twitter
-  .getTweets()
-  .then(tweets => {
-    const tweet = _.pick(_.first(tweets), 'created_at', 'id_str', 'text');
-    console.log('Twitter connection succeeeded');
-    console.log(JSON.stringify(tweet, null, 2));
-  })
-  .catch(err => {
-    console.log('Twitter connection failed', err);
-  });
+if (twitter.canConnect()) {
+  twitter
+    .getPosts()
+    .then(posts => {
+      const post = _.pick(_.first(posts), 'created_at', 'id_str', 'text');
+      console.log('Twitter connection succeeeded');
+      console.log(JSON.stringify(post, null, 2));
+    })
+    .catch(err => {
+      console.log('Twitter connection failed', err);
+    });
+}
+
+if (tumblr.canConnect()) {
+  tumblr
+    .getPosts()
+    .then(posts => {
+      const post = _.pick(_.first(posts), 'date', 'id', 'post_url', 'tags');
+      console.log('Tumblr connection succeeeded');
+      console.log(JSON.stringify(post, null, 2));
+    })
+    .catch(err => {
+      console.log('Tumblr connection failed', err);
+    });
+}
--- bin/tweet.js
+++ bin/tweet.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const dropbox = require('../lib/dropbox');
 const twitter = require('../lib/twitter');
+const tumblr = require('../lib/tumblr');
 const fs = require('fs');
 const path = require('path');
 
@@ -11,20 +12,38 @@ console.log(`Tweetin'...`);
 dropbox
   .getRandomImage()
   .then(image => {
-    twitter
-      .tweet(new Buffer(image.data, 'binary'))
-      .then(tweet => {
-        const tweetURL = `https://twitter.com/${
-          tweet.user.screen_name
-        }/status/${tweet.id_str}`;
-        console.log(`That tweet went OK! ${tweetURL}`);
-        dropbox.markAsTweeted(image, tweetURL).then(() => {
-          console.log(`Cool, "${image.name}" marked as tweeted`);
+    const binary = new Buffer(image.data, 'binary');
+    const basename = path.basename(image.name);
+    const matches = basename.match(/(.*) @ (.*)$/);
+    const episodeName = matches[1];
+
+    if (tumblr.canConnect()) {
+      tumblr.post(binary, episodeName)
+        .then((post) => {
+          console.log('Tumbl\'d', post);
+        }).catch(error => {
+          console.log('Did not post to Tumblr:', error);
         });
-      })
-      .catch(error => {
-        console.log('That tweet did NOT go OK:', error);
-      });
+    }
+
+    if (twitter.canConnect()) {
+      twitter
+        .post(binary)
+        .then(tweet => {
+          const tweetURL = `https://twitter.com/${
+            tweet.user.screen_name
+            }/status/${tweet.id_str}`;
+
+          console.log(`That tweet went OK! ${tweetURL}`);
+
+          dropbox.markAsTweeted(image, tweetURL).then(() => {
+            console.log(`Cool, "${image.name}" marked as tweeted`);
+          });
+        })
+        .catch(error => {
+          console.log('That tweet did NOT go OK:', error);
+        });
+    }
   })
   .catch(error => {
     console.log('Could get random image from dropbox:', error);
