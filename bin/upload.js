4521d8af2cb47468316454dd28ca96a7e31ecedb
Shah Kashani <skashani@gmail.com>
2018-06-22 23:29:35 -0400

    Uploads

---
+++ bin/upload.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+require('dotenv').config();
+
+const path = require('path');
+const glob = require('glob');
+const dropbox = require('../lib/dropbox');
+const fs = require('fs');
+
+const STILLS_FOLDER = path.resolve('./stills');
+
+const files = glob.sync(`${STILLS_FOLDER}/*.png`);
+
+const uploadSeries = files => {
+  if (files.length === 0) {
+    console.log('Donezo!');
+    return;
+  }
+  const file = files.shift();
+  console.log(`Uploading ${file}...`);
+  dropbox.uploadPhoto(file).then(() => {
+    fs.unlinkSync(file);
+    uploadSeries(files);
+  });
+};
+
+uploadSeries(files);
