1ae1f57852c85b8fc7d8dbea6d2f856faa4a2172
Shah Kashani <skashani@gmail.com>
2019-02-17 11:28:57 -0500

    Fix Tumblr GIF upload

--- bin/tweet.js
+++ bin/tweet.js
@@ -10,40 +10,38 @@ console.log(`Tweetin'...`);
 
 dropbox
   .getRandomImage()
-  .then(image => {
+  .then(async image => {
     const binary = new Buffer(image.data, 'binary');
     const basename = path.basename(image.name);
     const matches = basename.match(/(.*) @ (.*)$/);
     const episodeName = matches[1];
+    let tweetURL;
 
     if (twitter.canConnect()) {
-      twitter
-        .post(binary)
-        .then(tweet => {
-          const tweetURL = `https://twitter.com/${
-            tweet.user.screen_name
-          }/status/${tweet.id_str}`;
-
-          console.log(`That tweet went OK! ${tweetURL}`);
-
-          dropbox.markAsTweeted(image, tweetURL).then(() => {
-            console.log(`Cool, "${image.name}" marked as tweeted`);
-          });
-        })
-        .catch(error => {
-          console.log('That tweet did NOT go OK:', error);
-        });
+      try {
+        const tweet = await twitter.post(binary);
+        tweetURL = `https://twitter.com/${tweet.user.screen_name}/status/${
+          tweet.id_str
+        }`;
+        console.log(`That tweet went OK! ${tweetURL}`);
+      } catch (err) {
+        console.log('That tweet did NOT go OK:', error);
+      }
     }
 
     if (tumblr.canConnect()) {
-      tumblr
-        .post(binary, episodeName)
-        .then(post => {
-          console.log("Tumbl'd", post);
-        })
-        .catch(error => {
-          console.log('Did not post to Tumblr:', error);
-        });
+      try {
+        const post = await tumblr.post(binary, episodeName);
+        console.log("Tumbl'd", post);
+      } catch (err) {
+        console.log('Did not post to Tumblr:', error);
+      }
+    }
+
+    if (tweetURL) {
+      dropbox.markAsTweeted(image, tweetURL).then(() => {
+        console.log(`Cool, "${image.name}" marked as tweeted`);
+      });
     }
   })
   .catch(error => {
