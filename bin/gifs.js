12f8328345ce4c94bed2dff8f51a6ffcc8275667
Shah Kashani <skashani@gmail.com>
2019-02-17 11:36:26 -0500

    Support for GIF widths

--- bin/gifs.js
+++ bin/gifs.js
@@ -7,10 +7,26 @@ const path = require('path');
 const VIDEO_FOLDER = path.resolve('./videos');
 const GIFS_FOLDER = path.resolve('./gifs');
 
-const count = parseInt(process.argv[2], 10) || 5;
+const argv = require('yargs')
+  .usage('Usage: $0 <command> [options]')
+  .default('num', 5)
+  .default('width', 560).argv;
+
+const num = argv.num;
+const width = argv.width;
+
+console.log(`Making ${num} GIFs at ${width}px...`);
 
 screenshot
-  .makeGifs('**/*.{mp4,avi,mov,mkv}', VIDEO_FOLDER, GIFS_FOLDER, count, 0.2, 0.8)
+  .makeGifs(
+    '**/*.{mp4,avi,mov,mkv}',
+    VIDEO_FOLDER,
+    GIFS_FOLDER,
+    num,
+    0.2,
+    0.8,
+    width
+  )
   .then(files => {
     console.log(`Done making ${files.length} GIFs!`);
   })
